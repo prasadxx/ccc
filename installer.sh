5da70c04bb1abdc107202fef7fa4249adb47a8f3
prasadxx <106694802+prasadxx@users.noreply.github.com>
2022-09-09 15:10:18 +0530

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,112 @@
+REPO="REPO="https://github.com/TeamUltroid/Ultroid.git"
+DIR="/root/prasadxx"
+
+spinner(){
+    local pid=$!
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ];
+    do
+        for i in "Ooooo" "oOooo" "ooOoo" "oooOo" "ooooO" "oooOo" "ooOoo" "oOooo" "Ooooo"
+        do
+          echo -ne "\r• $i"
+          sleep 0.2
+        done
+    done
+}
+
+clone_repo(){
+    if [ ! $BRANCH ]
+        then export BRANCH="main"
+    fi
+    if [ -d $DIR ]
+        then
+            echo -e $DIR "Already exists.."
+            cd $DIR
+            git pull
+            currentbranch="$(git rev-parse --abbrev-ref HEAD)"
+            if [ currentbranch != $BRANCH ]
+                then
+                    git checkout $BRANCH
+            fi
+            if [ -d "addons" ]
+                then
+                    cd addons
+                    git pull
+            fi
+            return
+    fi
+    echo -e "Cloning Ultroid ${BRANCH}... "
+    git clone -b $BRANCH $REPO $DIR
+}
+
+install_requirements(){
+    echo -e "\n\nInstalling requirements... "
+    pip3 install -q --no-cache-dir -r $DIR/requirements.txt
+}
+
+
+
+
+
+
+main(){
+    (clone_repo)
+    (install_requirements)
+}
+
+main"
+DIR="/root/prasadxx"
+
+spinner(){
+    local pid=$!
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ];
+    do
+        for i in "Ooooo" "oOooo" "ooOoo" "oooOo" "ooooO" "oooOo" "ooOoo" "oOooo" "Ooooo"
+        do
+          echo -ne "\r• $i"
+          sleep 0.2
+        done
+    done
+}
+
+clone_repo(){
+    if [ ! $BRANCH ]
+        then export BRANCH="main"
+    fi
+    if [ -d $DIR ]
+        then
+            echo -e $DIR "Already exists.."
+            cd $DIR
+            git pull
+            currentbranch="$(git rev-parse --abbrev-ref HEAD)"
+            if [ currentbranch != $BRANCH ]
+                then
+                    git checkout $BRANCH
+            fi
+            if [ -d "addons" ]
+                then
+                    cd addons
+                    git pull
+            fi
+            return
+    fi
+    echo -e "Cloning prasadxx ${BRANCH}... "
+    git clone -b $BRANCH $REPO $DIR
+}
+
+install_requirements(){
+    echo -e "\n\nInstalling requirements... "
+    pip3 install -q --no-cache-dir -r $DIR/requirements.txt
+}
+
+
+
+
+
+
+
+main(){
+    (clone_repo)
+    (install_requirements)
+}
+
+main
